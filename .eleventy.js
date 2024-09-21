18570a835c8dfce6c0c594e02e146f2e6e4e421f
UWM-YDSA <uwmdsa@gmail.com>
2024-09-21 20:09:21 +0000

    🔄 created local '.eleventy.js' from remote '.eleventy.js'

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./admin/");
+    eleventyConfig.addPassthroughCopy("./static/");
+    eleventyConfig.addPassthroughCopy("./LICENSE");
+    eleventyConfig.addPassthroughCopy("./CNAME");
+    
+    // Defaults
+    eleventyConfig.addGlobalData( "favicon-32", "/static/img/icon/favicon-32.ico");
+    eleventyConfig.addGlobalData("favicon-192", "/static/img/icon/favicon-192.ico");
+    eleventyConfig.addGlobalData(        "org", "Milwaukee YDSA");
+    eleventyConfig.addGlobalData(  "shortcode", "MKE");
+    eleventyConfig.addGlobalData(       "logo", "/static/img/icon/logo.svg")
+    eleventyConfig.addGlobalData(   "hero.img", "/static/img/hero/index.png");
+    eleventyConfig.addGlobalData( "hero.color", "red-tint-1");
+
+    // Shortcodes
+    eleventyConfig.addShortcode(
+        "sectitle",
+        (title) =>
+          `<h2>${title}</h2>
+            <hr>`
+    );
+
+    return {
+        dir: {
+            input: "src",
+            output: "dist",
+        },
+    };
+};
