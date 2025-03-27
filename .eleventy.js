101671ac0d1ca96d4b40d60522a7b440a269968a
UWM-YDSA <uwmdsa@gmail.com>
2025-03-27 21:04:13 +0000

    🔄 synced local '.eleventy.js' with remote '.eleventy.js'

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,35 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const externalLinks = require('eleventy-plugin-external-links');
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./admin/");
     eleventyConfig.addPassthroughCopy("./static/");
     eleventyConfig.addPassthroughCopy("./LICENSE");
     eleventyConfig.addPassthroughCopy("./CNAME");
+    eleventyConfig.addPassthroughCopy({ './src/robots.txt': '/robots.txt' });
+    eleventyConfig.addPassthroughCopy({ './src/ai.txt': '/ai.txt' });
     
-    // Defaults
-    eleventyConfig.addGlobalData( "favicon-32", "/static/img/icon/favicon-32.ico");
-    eleventyConfig.addGlobalData("favicon-192", "/static/img/icon/favicon-192.ico");
-    eleventyConfig.addGlobalData(        "org", "Milwaukee YDSA");
-    eleventyConfig.addGlobalData(  "shortcode", "mke");
-    eleventyConfig.addGlobalData(       "logo", "/static/img/icon/logo.svg")
-    eleventyConfig.addGlobalData(   "hero.img", "/static/img/hero/index.png");
-    eleventyConfig.addGlobalData( "hero.color", "red-tint-1");
+    // Global Variables
+    eleventyConfig.addGlobalData("baseurl", "https://mkeydsa.org");
+    eleventyConfig.addGlobalData("chapters", [
+        {
+            "name": "University of Wisconsin-Milwaukee",
+            "shortcode": "uwm",
+            "link": "https://uwm.mkeydsa.org",
+            "interest": "https://uwm.mkeydsa.org/join"
+        },
+        {
+            "name": "Milwaukee School of Engineering",
+            "shortcode": "msoe",
+            "link": "https://www.instagram.com/msoe_ydsa/",
+        },
+        {
+            "name": "Marquette University",
+            "shortcode": "marquette",
+            "link": "https://www.instagram.com/ydsa.marquette/",
+            "interest": "https://forms.gle/F3gaq7CgMpRnoy8J8"
+        }
+    ]);
 
     // Shortcodes
     eleventyConfig.addShortcode(
@@ -20,6 +38,42 @@ module.exports = function (eleventyConfig) {
           `<h2>${title}</h2>
             <hr>`
     );
+
+    // Plugin config
+    eleventyConfig.addPlugin(pluginRss);
+
+    eleventyConfig.addPlugin(externalLinks, {
+        name: 'external-links',
+        regex: /\bhttps?:\/\/(?!([a-z]+\.)?mkeydsa\.org)[^\s\/]+(?:\/[^\s\/]+)*\/?/i,  // Regex that test if href is external
+        target: "_blank",               // 'target' attribute for external links
+        rel: "noopener",                // 'rel' attribute for external links
+        extensions: [".html"],          // Extensions to apply transform to
+        includeDoctype: true,           // Default to include '<!DOCTYPE html>' at the beginning of the file
+    });
+
+    // thank you Martin Gunnarsson
+    // https://www.martingunnarsson.com/posts/eleventy-excerpts/
+    eleventyConfig.addFilter("excerpt", function (content, length) {
+        if (length <= 0) length = 200;
+
+        let excerptParagraphs = [];
+        let currentLength = 0;
+        const paragraphs = content.match(/<p>.*?<\/p>/gs) || [];
+
+        for (let paragraph of paragraphs) {
+            // Strip HTML from the paragraph
+            const text = paragraph.replace(/(<([^>]+)>)/gi, "");
+
+            if (currentLength > 0 && currentLength + text.length > length) {
+                break;
+            }
+
+            excerptParagraphs.push(text);
+            currentLength += text.length;
+        }
+
+        return excerptParagraphs.join(" ");
+    });
 
     return {
         dir: {
